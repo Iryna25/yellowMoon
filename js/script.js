9dffb3b0b00aaea35ffae98b107d97093cd6399d
Irina <italia2505@gmail.com>
2017-03-20 12:09:15 +0200

    add script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-﻿
+﻿$(function(){
 
+$ ('.landing .arrow a').click(function(e){
+	e.preventDefault();	
+	var href=$(this).attr('href'), speed=1200;
+
+ $('html, body').animate({ scrollTop: $(href).offset().top }, speed);
+});
+
+
+
+ $('.navbar-toggle').click(function(e){
+ e.preventDefault();
+
+	var target = $(this).attr('href');
+	$(target).addClass('collapse');
+})
 
  
-  
+})
